60367f9b9b4afffb211304681b38f029cff9bb25
jack <whisler1@gmail.com>
2025-09-22 15:24:00 -0500

    more problems

--- jwhisler_hw_3_3.py
+++ jwhisler_hw_3_3.py
@@ -5,10 +5,29 @@ Class: CS 521 - Fall
 
 Date: 9/18/2025
 
-Homework Problem # 3_2
+Homework Problem # 3_3
 
 Description: Allow user to order item off the menu, by number name
 """
 MENU = ["ham", "eggs", "bacon", "fish", "toast", "spam", "fruit"]
 
 print("Welcome to the Breakfast Café!")
+
+print("Please select a menu item.")
+order = ""
+while order.lower() not in MENU:
+  for i in range(0, len(MENU)):
+      print(f"{i + 1}. {MENU[i]}")
+
+  order = input("Your order? ")
+  try:
+    order_number = int(order)
+    if MENU[order_number - 1]:
+      order = MENU[order_number - 1]
+  except:
+    pass
+  if order.lower() not in MENU: 
+    print("Sorry, I don't think we carry that.")
+  else:
+    print(f"One order of {order} coming right up!")
+    break
