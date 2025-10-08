6510e05e5d429fd2d1ca61c55a9edb411bf17db0
Jack Whisler <78163023+jackwhisler1@users.noreply.github.com>
2025-10-08 15:47:03 -0500

    updates to week 6

--- jwhisler_6/Sentence.py
+++ jwhisler_6/Sentence.py
@@ -9,14 +9,47 @@ Homework Problem # 6
 
 Description: Class library for Sentence that evaluates and manipulates an English sentence.
 """
+import string
+
 
 class Sentence:
-  def __init__(self, sentence_str):
-    self.sentence = sentence_str
+    """"""
 
-  def __str__(self):
-    return 'Your sentence: {}'.format(self.sentence.upper())
+    def __init__(self, sentence_str=''):
+        """"""
+        words_list = sentence_str.strip(string.punctuation).split(' ')
+        self.__sentence = words_list
+
+    def __repr__(self):
+        return "{}.".format(' '.join(self.__sentence))
+
+    def get_all_words(self):
+        """Returns a list of words from sentencee"""
+
+        return self.__sentence
+
+    def get_word(self, index):
+        """Returns word at specified index or empty string if outside of range"""
+        if 0 <= index < len(self.__sentence):
+            return self.__sentence[index]
+        return ''
+
+    def set_word(self, index, new_word):
+        """Changes the word at a given index location in sentence to a new word"""
+        if index >= len(self.__sentence):
+            self.__sentence.append(new_word)
+        else:
+            self.__sentence[index] = new_word
+
+    def scramble(self):
+        """"""
 
 
 s = Sentence("La la la testing out.")
+
+print(s.get_all_words())
+print(s.get_word(3))
+print(s.get_word(5))
+print(s.set_word(5, "newword"))
+print(s.get_word(3))
 print(s)
